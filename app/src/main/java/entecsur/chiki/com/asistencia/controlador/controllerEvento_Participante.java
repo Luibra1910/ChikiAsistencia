58b20360af96fd974013314fecf75b2eeb304d42
Luibra1910 <luibra1910@gmail.com>
2016-07-21 17:09:01 -0500

    Commit 21-07-16 new

--- app/src/main/java/entecsur/chiki/com/asistencia/controlador/controllerEvento_Participante.java
+++ app/src/main/java/entecsur/chiki/com/asistencia/controlador/controllerEvento_Participante.java
@@ -7,9 +7,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -17,7 +19,6 @@ import java.util.ArrayList;
 import entecsur.chiki.com.asistencia.FragmentEvento;
 import entecsur.chiki.com.asistencia.activityPrincipal;
 import entecsur.chiki.com.asistencia.bean.Evento;
-import entecsur.chiki.com.asistencia.bean.Evento_Participante;
 import entecsur.chiki.com.asistencia.bean.Persona;
 
 public class controllerEvento_Participante {
@@ -35,47 +36,28 @@ public class controllerEvento_Participante {
 
     public Persona verificarParticipante(String dni, String evento){
         Persona per = null;
-        JsonArrayRequest array = new JsonArrayRequest(Request.Method.GET,
+
+        JsonObjectRequest objeto = new JsonObjectRequest(Request.Method.GET,
                 "http://www.json-generator.com/api/json/get/cqSDBodYEi?indent=2",
-                new Response.Listener<JSONArray>() {
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        try {
-                            if (response.length() > 0) {
-                                ArrayList<Evento> events = new ArrayList<>();
-                                Evento evento = null;
-                                for (int i = 0; i < response.length(); i++) {
-                                    JSONObject object   = response.getJSONObject(i);
-
-                                    String idEvento     = object.getString("idEvento");
-                                    String hora         = object.getString("hora");
-                                    String fecha        = object.getString("fecha");
-                                    String precio       = object.getString("precio");
-                                    String categoria    = object.getString("categoria");
-                                    String nombre       = object.getString("nombre");
-                                    String ambiente     = object.getString("ambiente");
-
-                                    evento = new Evento(idEvento, hora, fecha, precio,
-                                            categoria, nombre, ambiente);
-                                    System.out.println("Name: "+ evento.getNombreEvento());
-                                    events.add(evento);
-                                }
-                                //marcarAsistencia(events);
+                    public void onResponse(JSONObject response) {
+                        if(response!=null){
+                            try {
+                                String nombre = response.getString("nombre");
+                            } catch (JSONException e) {
+                                Log.d("Error Verificacion: ",e.getMessage());
                             }
-                        } catch (Exception e) {
-                            Log.d("Excepetion", e.getMessage());
                         }
                     }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+            }
+        });
 
-                    }
-                });
-
-        req.add(array);
+        req.add(objeto);
         return per;
     }
 
